15acc36792aa7408ecb6c1a8d7db714415f29921
Krizerion <kris.petrov.93@gmail.com>
2024-02-16 15:25:01 +0200

    Project setup

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'qs-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'questie';
+  test = 'test3';
+  private test2 = 'test';
+
+  doSomethingss(): void {
+    console.log('test');
+  }
+
+  ngOnInit(): void {
+    console.log(5);
+  }
+
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  private testMethod(): number {
+    return 5;
+  }
+}
